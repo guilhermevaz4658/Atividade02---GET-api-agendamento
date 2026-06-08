d89b1f97cae9aafc3c7dccb6f0a1808cdce6d119
GUILHERME SILVEIRA VAZ <04626245013@senacrs.edu.br>
2026-06-08 16:56:16 -0300

    feature: rota post

--- server.js
+++ server.js
@@ -19,29 +19,59 @@ app.get('/api/user/:email', async (req, res) => {
         ) */
 
         //Sem SQL Injection
-         const getUser = await conexao.query(
-            "SELECT * FROM usuario WHERE email = ?" , 
+        const getUser = await conexao.query(
+            "SELECT * FROM usuario WHERE email = ?",
             [userEmail]
-        )               
+        )
         res.send(getUser[0])
-    } catch(err){
+    } catch (err) {
         console.log('Erro ao consultar MySQL:')
         console.log(err.message);
     }
 })
 
-app.get('/api/agendamentos', async (req, res) =>{
+app.get('/api/agendamentos', async (req, res) => {
     try {
         const agendamentos = await conexao.query(
             "SELECT agendamentos.id, agendamentos.data_agendamento, usuarios.nome AS usuario_nome, servicos.nome AS servico_nome, servicos.preco FROM agendamentos INNER JOIN usuarios ON agendamentos.usuario_id = usuarios.id INNER JOIN servicos ON agendamentos.servico_id = servicos.id ORDER BY agendamentos.data_agendamento ASC;"
         )
         res.send(agendamentos[0])
     } catch (error) {
-        console.log('Erro ao consultar MySQL:')
-        console.log(error.message)
+        res.status(500).json({
+            erro: "Erro ao conectar com MySQL",
+            mensagem: error.message
+        })
     }
 })
 
+app.post('/api/criarAgendamento', async (req, res) => {
+    try {
+        const { date, usuarioID, servicoID } = req.body;
+        if (!usuarioID || !servicoID || !date) {
+            res.send({ erro: "Usuário serviço e data precisam ser informados" })
+        }
+        const [usuario] = await conexao.query("SELECT * FROM usuarios WHERE id = ?", [usuarioID])
+        if (usuario.length === 0) {
+            return res.status(404).json({ erro: "Usuário não encontrado" })
+        }
+        const [servico] = await conexao.query("SELECT * FROM servicos WHERE id = ?", [servicoID])
+        if (servico.length === 0) {
+            return res.status(404).json({ erro: "Serviço não encontrado" })
+        }
+        const [novoAgendamento] = await conexao.query("INSERT INTO agendamentos (data_agendamento, usuario_id, servico_id) VALUES (?, ?, ?)", [date, usuarioID, servicoID])
+
+        res.status(201).json({
+            mensagem: "Agendamento criado com sucesso",
+            id: novoAgendamento.insertId
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            erro: "Erro ao conectar com MySQL",
+            mensagem: error.message
+        })
+    }
+})
 
 app.listen(port, () => {
     console.log(`Server rodando em http://localhost:${port}`)
